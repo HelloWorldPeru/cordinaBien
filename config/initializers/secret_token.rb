4d88b413d28bab97de0d6e3349408656d65e6139
jonathancg90 <jonathancg90@gmail.com>
2014-01-05 22:55:08 -0500

    Init aplication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CordinaBien::Application.config.secret_key_base = '8ae46c21c12f44d64d716c93462c11668ac023e4d5567ac3d6372d3fdecdb7b03e57ea37f12aa6abcb8dc59fdceff5f114eed6daab5d29f61cf355a15ed40beb'
